01fc01951e8cac0238c800aecd07a7bd02faa0e8
yuraha18 <yura.yush@gmail.com>
2017-05-23 20:23:07 +0300

    Get test photo from server

---
+++ app/src/main/java/com/yuraha18/simplephotoviewer/DTO/PhotoLinks.java
@@ -0,0 +1,8 @@
+package com.yuraha18.simplephotoviewer.DTO;
+
+/**
+ * Created by User on 5/23/2017.
+ */
+
+public class PhotoLinks {
+}
---
+++ app/src/main/java/com/yuraha18/simplephotoviewer/UnsplashAPI/URL.java
@@ -0,0 +1,8 @@
+package com.yuraha18.simplephotoviewer.UnsplashAPI;
+
+/**
+ * Created by User on 5/23/2017.
+ */
+
+public class URL {
+}
